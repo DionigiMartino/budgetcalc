f9094172c84a822754f9db20951fcd8c79b20209
DionigiMartino <dionigi49@gmail.com>
2020-09-18 15:29:04 +0200

    Set the Budget Displayer

---
+++ src/Components/Budget/Budget.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Budget.module.css'
+import Aux from '../../Hoc/Hoc';
+
+const Budget = props => {
+
+    let earn = props.earning.reduce((a, b) => {
+        return a + b
+    })
+
+    let exp = props.expenses.reduce((a, b) => {
+        return a + b
+    });
+
+    let total = props.total;
+
+    // eslint-disable-next-line
+    let classes = []
+
+    return(
+        <Aux>
+            <div className={styles.BudgetCont}>
+                <h2>Budget Calculator</h2>
+
+                <div className={styles.BudgetDisplay}>
+                    <p className={styles.Earn}>Earnings: {earn}</p>
+
+                    <p className={earn > exp ? classes = [styles.Total, styles.Good].join(' ') : classes = [styles.Total, styles.Bad].join(' ')}>Total: {total}</p>
+
+                    <p className={styles.Exp}>Expenses: {exp}</p>
+                </div>
+            </div>
+        </Aux>
+    )
+}
+
+export default Budget;
--- src/Components/ContainerApp/ContainerApp.js
+++ src/Components/ContainerApp/ContainerApp.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react'
 import styles from './ContainerApp.module.css'
 
 import Aux from '../../Hoc/Hoc'
+import Budget from '../Budget/Budget'
 
 class ContainerApp extends Component {
     constructor(props){
         super(props);
         this.state = {
-            optionValue: '+'
+            optionValue: '+',
+            earn: [14, 21, 214, 123 ],
+            exp: [11, 5, 8, 91],
+            total: null
         }
         this.changeOption = this.changeOption.bind(this)
     }
@@ -20,18 +24,52 @@ class ContainerApp extends Component {
         }
     }
 
+    componentDidMount(){
+        this.updateBudget()
+    }
+
+    updateBudget(){
+        let earning = this.state.earn.reduce((a, b) => {
+            return a + b;
+        });
+
+        let expenses = this.state.exp.reduce((a, b) => {
+            return a + b;
+        })
+
+        let totalBudget = earning - expenses;
+
+        this.setState({
+            total: totalBudget
+        })
+    }
+
+    addCount(e){
+        e.preventDefault();
+
+        if(this._inputNewValue.value !== ''){
+            console.log(this._inputNewValue.value);
+            this._inputNewValue.value = ''
+        } else {
+            alert('Inserisci un valore')
+        }
+    }
+
     render(){
+        // eslint-disable-next-line
         let classes = [];
 
         return(
             <Aux>
+                <Budget earning={this.state.earn} expenses={this.state.exp} total={this.state.total}/>
                 <div className={styles.ContainerWhole}>
-                    <form onSubmit={this.addCount} className={styles.FormCont}>
+                    <form onSubmit={(e) => this.addCount(e)} className={styles.FormCont}>
                         <select onChange={this.changeOption} ref={(a) => this._inputElement = a} className={this.state.optionValue === '+' ? classes = [styles.selectValue, styles.Green].join(' ') : classes = [styles.selectValue, styles.Red].join(' ')}>
                             <option>+</option>
                             <option>-</option>
                         </select>
-                        <input name="inputValue" className={this.state.optionValue === '+' ? classes = [styles.inputValue, styles.Green].join(' ') : classes = [styles.inputValue, styles.Red].join(' ')} placeholder="Add the value..." />
+                        <input ref={(a) => this._inputNewValue = a} className={this.state.optionValue === '+' ? classes = [styles.inputValue, styles.Green].join(' ') : classes = [styles.inputValue, styles.Red].join(' ')} placeholder="Add the value..." />
+                        <input type="number" placeholder="Add Number..." ref={(a) => this._inputNumber = a} className={this.state.optionValue === '+' ? classes = [styles.numberValue, styles.Green].join(' ') : classes = [styles.numberValue, styles.Red].join(' ')}/>
                         <input type="submit" className={this.state.optionValue === '+' ? classes = [styles.ButtonSubmit, styles.Green].join(' ') : classes = [styles.ButtonSubmit, styles.Red].join(' ')} value="Add" />
                     </form>
                 </div>
